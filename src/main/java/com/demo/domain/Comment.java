118c30d75ffe7407a5659eb4497f7ef3f1a13ff3
yo-tanaka2 <yo-tanaka2@10.231.251.196>
2018-12-28 14:01:58 +0900

    define Comment

---
+++ src/main/java/com/demo/domain/Comment.java
@@ -0,0 +1,26 @@
+package com.demo.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "comments", indexes = @Index(columnList = "comments"))
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Comment {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	@Column
+	private String comment;
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "pickId")
+	private Pick pick;
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "userId")
+	private User user;
+	
+}
