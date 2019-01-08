c23dd2a7aa36d3419d6cb43166d333f86549f7a1
yosukenn <yo-tanaka2@unext.jp>
2019-01-08 16:21:34 +0900

    add comment to ThmeRepository

--- src/main/java/com/demo/repository/ThemeRepository.java
+++ src/main/java/com/demo/repository/ThemeRepository.java
@@ -4,6 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.demo.domain.Theme;
 
+/**
+ * テーマのリポジトリ
+ * 
+ * @author yosukenn
+ *
+ */
 @Repository
 public interface ThemeRepository extends JpaRepository<Theme, Integer> {
 
