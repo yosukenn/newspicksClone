1dee25b1d38c8348fd84f0bfeec529f6e36f0b0b
yosukenn <yo-tanaka2@unext.jp>
2019-01-15 19:09:11 +0900

    create search method

--- src/main/java/com/demo/web/PickController.java
+++ src/main/java/com/demo/web/PickController.java
@@ -4,6 +4,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import com.demo.domain.Pick;
 import com.demo.service.PickService;
@@ -21,13 +23,40 @@ public class PickController {
 	PickService pickService;
 	
 	/**
+	 * 検索フォームクラスを初期化するメソッド。返り値を自動的にModel に追加する
+	 * @return 検索フォームのパラメータをバインドする PickSearchForm クラス
+	 */
+	@ModelAttribute
+	PickSearchForm setUpForm() {
+		return new PickSearchForm();
+	}
+	
+	/**
+	 * トップページを表示するメソッド。
 	 * @param model 画面に渡す値をModelオブジェクトの属性に設定
 	 * @return 遷移する画面（ニュースを表示したトップページ）の名前
 	 */
 	@GetMapping
-	String list(Model model) {
+	String index(Model model) {
 		List<Pick> picks = pickService.findAll();
 		model.addAttribute("picks", picks);
 		return "picks/index";
+	}
+	
+	/**
+	 * ユーザが入力したキーワードから検索画面を表示させるメソッド。
+	 * @param keyword ユーザが入力したキーワード
+	 * @param result キーワードをバリデーションにかけた結果が格納される
+	 * @param model 画面に渡す値をModelオブジェクトの属性に設定
+	 * @return 検索結果を表示する画面のパス
+	 */
+	@GetMapping(path = "search")
+	String search(@Validated PickSearchForm keyword, BindingResult result, Model model) {
+		if (result.hasErrors()) {
+			return index(model);
+		}
+		List<Pick> picks = pickService.findByKeyword(keyword);
+		model.addAttribute("picks", picks);
+		return "picks/search";
 	}
 }
