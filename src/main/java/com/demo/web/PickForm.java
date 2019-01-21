72ac83e3ad3d7542ec3a206407cc6124a23c481c
yosukenn <yo-tanaka2@unext.jp>
2019-01-21 11:05:50 +0900

    add describe

--- src/main/java/com/demo/web/PickForm.java
+++ src/main/java/com/demo/web/PickForm.java
@@ -4,6 +4,11 @@ import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
+/**
+ * 記事をピックするときにユーザが入力したURLがバインドされるクラス
+ * @author yo-tanaka2
+ *
+ */
 @Data
 public class PickForm {
 	@NotNull
