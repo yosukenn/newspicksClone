42f8b245ad85ecf994c1fb1b0a3a749330392b4c
yosukenn <yo-tanaka2@unext.jp>
2019-01-21 13:01:42 +0900

    add comment

--- src/main/java/com/demo/web/PickController.java
+++ src/main/java/com/demo/web/PickController.java
@@ -94,7 +94,7 @@ public class PickController {
 	
 	/**
 	 * IOException の例外ハンドラー
-	 * 暫定的な処置としてJSONを返す
+	 * Ajax でフォームにエラーメッセージを表示したいが、暫定的な処置としてJSONを返す
 	 * @param e エラーオブジェクト
 	 * @return エラー時のJSONメッセージ
 	 */
